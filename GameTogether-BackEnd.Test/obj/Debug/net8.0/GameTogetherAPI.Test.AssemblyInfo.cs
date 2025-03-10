e825908509a72d119ebbd49267c94e1783f6f624
Freb <frederik.bode@hendrichsen.d>
2025-03-10 10:26:02 +0100

    Revert "More test dirs"
    This reverts commit 14b818cb9eba123d4c1e34b528f5673e4e677992.

--- GameTogether-BackEnd.Test/obj/Debug/net8.0/GameTogetherAPI.Test.AssemblyInfo.cs
+++ GameTogether-BackEnd.Test/obj/Debug/net8.0/GameTogetherAPI.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameTogetherAPI.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d21369ae8e65f404f055e90c0a62141fcb5b664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580fb927b2f378645c00d0d75b14b3342aee8b2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameTogetherAPI.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameTogetherAPI.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
