a565d130f8eab4df2442b857efd5f376dbc762e3
Frebbers <frederik.bode@hendrichsen.dk>
2025-03-11 19:15:13 +0100

    Test classes

---
+++ GameTogetherAPI.Test/StepDefinitions/TestingUserManagementStepDefinitions.cs
@@ -0,0 +1,28 @@
+﻿using GameTogetherAPI.Test.Drivers;
+using GameTogetherAPI.Test.Fixtures;
+using GameTogetherAPI.Test.Hooks;
+
+namespace GameTogetherAPI.Test.StepDefinitions;
+using FluentAssertions;
+
+[Binding]
+public class TestingUserManagementStepDefinitions(ScenarioContext scenarioContext)
+{
+    private readonly ScenarioContext scenarioContext = scenarioContext;
+
+    [Given(@"I send a create account request")]
+    public async Task GivenISendACreateAccountRequest()
+    {
+        APIDriver driver = new APIDriver(TestHooks.Context.Client);
+        var response = await driver.SendRequest($"/api/auth/register", HttpMethod.Post, new { email = "user@example.com", Password = "Password123" });
+        scenarioContext.Add("response", response);
+    }
+
+    [Then(@"I assert that the account is created")]
+    public void ThenIAssertThatTheAccountIsCreated()
+    {
+        var response = scenarioContext.Get<HttpResponseMessage>("response");
+        var responseCode = response.StatusCode.ToString();
+        responseCode.Should().BeEquivalentTo("OK");
+    }
+}
