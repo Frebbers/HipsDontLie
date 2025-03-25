21ad2c42571d806448b995a71faa3a77a8e66034
Adrian Macauley <s225733@dtu.dk>
2025-03-25 14:25:42 +0100

    Implemented TestUser is reset function and changed a few Stepdefs around to optimize and verify testuser.

--- GameTogetherAPI.Test/StepDefinitions/TestingUserManagementStepDefinitions.cs
+++ GameTogetherAPI.Test/StepDefinitions/TestingUserManagementStepDefinitions.cs
@@ -14,7 +14,7 @@ public class TestingUserManagementStepDefinitions(ScenarioContext scenarioContex
     [Given(@"I send a create account request")]
     public async Task GivenISendACreateAccountRequest()
     {
-        APIDriver driver = new APIDriver(TestHooks.Context.Client);
+        var driver = new APIDriver(TestHooks.Context.Client);
         var response = await driver.SendPostRequest($"/api/auth/register", new RegisterModel(
             APIConstants.TestEmail, APIConstants.TestPassword)
         );
@@ -28,38 +28,54 @@ public class TestingUserManagementStepDefinitions(ScenarioContext scenarioContex
         var responseCode = response.StatusCode.ToString();
         responseCode.Should().BeEquivalentTo("OK");
     }
-        [Given("I send a log in request")]
-        public async Task GivenISendALogInRequest()
-        {
-        APIDriver driver = new APIDriver(TestHooks.Context.Client);
+    [Given("I send a log in request")]
+    public async Task GivenISendALogInRequest()
+    {
+        var driver = new APIDriver(TestHooks.Context.Client);
         var response = await driver.SendPostRequest($"/api/auth/login", new { Util.APIConstants.TestEmail, Util.APIConstants.TestPassword });
         scenarioContext.Add("response", response);
         Console.WriteLine(response);
     }
-
-        [Then("I assert that the account is logged in")]
-        public void ThenIAssertThatTheAccountIsLoggedIn()
-        {
-            var response = scenarioContext.Get<HttpResponseMessage>("response");
-            var responseCode = response.StatusCode.ToString();
-            responseCode.Should().BeEquivalentTo("OK");
+    
+    [Then("I assert that the account is logged in")]
+    public void ThenIAssertThatTheAccountIsLoggedIn()
+    {
+        var response = scenarioContext.Get<HttpResponseMessage>("response");
+        var responseCode = response.StatusCode.ToString();
+        responseCode.Should().BeEquivalentTo("OK");
             
     }
 
-        [Given(@"I am logged in")]
-        public async Task  GivenIAmLoggedIn()
-        {
-            APIDriver driver = new APIDriver(TestHooks.Context.Client);
-            var response = await driver.SendGetRequest("/api/auth/me");
-            var responseEmail = response.ToString();
-            var responseCode = response.StatusCode.ToString();
-            responseEmail.Should().BeEquivalentTo(Util.APIConstants.TestEmail);
-            responseCode.Should().BeEquivalentTo("OK");
-        }
+    [Given(@"I am logged in")]
+    public async Task  GivenIAmLoggedIn()
+    {
+        var driver = new APIDriver(TestHooks.Context.Client);
+        var response = await driver.SendGetRequest("/api/auth/login");
+        var responseToken = response.ToString();
+        var responseCode = response.StatusCode.ToString();
+            
+        responseCode.Should().BeEquivalentTo("OK");
+    }
+
 
         [Given(@"I click the log off button")]
         public void GivenIClickTheLogOffButton()
         {
             ScenarioContext.StepIsPending();
         }
+        
+        [Given(@"TestUser is reset")]
+        public async Task GivenTestUserIsReset()
+        {
+            var driver = new APIDriver(TestHooks.Context.Client); //du skal bruge driver fra din context
+            var loginresponse = await driver.SendGetRequest("/api/auth/login", new { Util.APIConstants.TestEmail, Util.APIConstants.TestPassword }); //du kan også genne dit login i context
+            scenarioContext.Add("response", loginresponse);
+            loginresponse.Headers.TryGetValues("token", out var responseToken);
+            scenarioContext["token"] = responseToken;
+            var deleteResponse = await driver.SendDeleteRequest("/api/auth/remove-user", responseToken);
+            deleteResponse.StatusCode.ToString().Should().BeEquivalentTo("OK");
+
+            //var response = scenarioContext.Get<HttpResponseMessage>("response");
+        }
+        
 }
