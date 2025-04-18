ec3e17a24fc56120f4993560720e5de428e24227
sjbrandt <144696315+sjbrandt@users.noreply.github.com>
2025-04-18 13:35:41 +0200

    Added nonUserMembers migration

---
+++ GameTogetherAPI/Migrations/20250418113417_addedNonUserMembers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameTogetherAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedNonUserMembers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NonUserMembers",
+                table: "Groups",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NonUserMembers",
+                table: "Groups");
+        }
+    }
+}
