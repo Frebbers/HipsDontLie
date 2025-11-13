275bd9f29dc8006710587557bb27ba408cffa9df
Mathias Hidan <MathiasHidan@outlook.com>
2025-11-13 23:35:47 +0100

    Several CRUD methods added to the resourceService.

--- HipsDontLie.Client/Services/ResourceService.cs
+++ HipsDontLie.Client/Services/ResourceService.cs
@@ -9,6 +9,9 @@ namespace HipsDontLie.Client.Services
 
         public ResourceService(IHttpClientFactory factory) => _http = factory.CreateClient("Api");
 
+        public record ApiResponseMessage(string Message);
+
+        #region Profile
         public async Task<ProfileDTO> GetProfile(int? userId = null, CancellationToken ct = default)
         {
             var requestUri = "api/Users/get-profile";
@@ -19,6 +22,47 @@ namespace HipsDontLie.Client.Services
             return await res.Content.ReadFromJsonAsync<ProfileDTO>(ct) ?? new();
         }
 
+        public async Task<ApiResponseMessage?> UpdateProfile(UpdateProfileRequestDTO profile, CancellationToken ct = default)
+        {
+            using var res = await _http.PostAsJsonAsync("api/Users/update-profile", profile, ct);
+            if (!res.IsSuccessStatusCode) return null;
+
+            return await res.Content.ReadFromJsonAsync<ApiResponseMessage?>(ct);
+        }
+        #endregion
+
+        #region Group
+        public async Task<ApiResponseMessage?> CreateGroup(CreateGroupRequestDTO group, CancellationToken ct = default)
+        {
+            using var res = await _http.PostAsJsonAsync("api/Groups/create", group, ct);
+            if (!res.IsSuccessStatusCode) return null;
+
+            return await res.Content.ReadFromJsonAsync<ApiResponseMessage>(ct);
+        }
+
+        public async Task<ApiResponseMessage?> JoinGroup(int groupId, CancellationToken ct = default)
+        {
+            using var res = await _http.PostAsync($"api/Groups/{groupId}/join", null, ct);
+            if (!res.IsSuccessStatusCode) return null;
+
+            return await res.Content.ReadFromJsonAsync<ApiResponseMessage>(ct);
+        }
+        public async Task<ApiResponseMessage?> AcceptUserIntoGroup(int groupId, int userId, CancellationToken ct = default)
+        {
+            using var res = await _http.GetAsync($"api/Groups/{groupId}/{userId}/accept", ct);
+            if (!res.IsSuccessStatusCode) return null;
+
+            return await res.Content.ReadFromJsonAsync<ApiResponseMessage>(ct);
+        }
+
+        public async Task<ApiResponseMessage?> RejectUserFromGroup(int groupId, int userId, CancellationToken ct = default)
+        {
+            using var res = await _http.GetAsync($"api/Groups/{groupId}/{userId}/reject", ct);
+            if (!res.IsSuccessStatusCode) return null;
+
+            return await res.Content.ReadFromJsonAsync<ApiResponseMessage>(ct);
+        }
+
         public async Task<List<GroupDTO>> GetUserGroups(CancellationToken ct = default)
         {
             using var res = await _http.GetAsync("api/Groups/user", ct);
@@ -26,7 +70,15 @@ namespace HipsDontLie.Client.Services
 
             return await res.Content.ReadFromJsonAsync<List<GroupDTO>>(ct) ?? new();
         }
-        
+
+        public async Task<List<GroupDTO>> GetAllGroups(CancellationToken ct = default)
+        {
+            using var res = await _http.GetAsync("api/Groups", ct);
+            if (!res.IsSuccessStatusCode) return new();
+
+            return await res.Content.ReadFromJsonAsync<List<GroupDTO>>(ct) ?? new();
+        }
+
         public async Task<GroupDTO> GetGroupByID(int groupId, CancellationToken ct = default)
         {
             using var res = await _http.GetAsync($"api/Groups/{groupId}", ct);
@@ -34,6 +86,10 @@ namespace HipsDontLie.Client.Services
 
             return await res.Content.ReadFromJsonAsync<GroupDTO>(ct) ?? new();
         }
+        #endregion
 
+        #region Chat
+        //TO-DO
+        #endregion
     }
 }
