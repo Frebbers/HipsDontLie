1ed7818e39286c38328ceebbffa7c95e24f1b96a
Mathias Hidan <MathiasHidan@outlook.com>
2025-11-25 16:21:39 +0100

    missed a slash

--- HipsDontLie.Client/Program.cs
+++ HipsDontLie.Client/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddTransient<ApiErrorHandler>();
 builder.Services.AddTransient<ApiRequestHandler>();
 
 var apiBaseUrl = builder.HostEnvironment.IsProduction()
-    ? "https://hipsdontlie.live/api/"
+    ? "https://hipsdontlie.live/"
     : "https://localhost:7191/";           
 
 builder.Services.AddHttpClient("Auth", c =>
