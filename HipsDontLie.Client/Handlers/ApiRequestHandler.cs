e0f63e9475c80c9db628650ae6ec4757f326a60c
Mathias Hidan <MathiasHidan@outlook.com>
2025-11-27 16:07:31 +0100

    Removed guard that would break app in localhost

--- HipsDontLie.Client/Handlers/ApiRequestHandler.cs
+++ HipsDontLie.Client/Handlers/ApiRequestHandler.cs
@@ -3,20 +3,15 @@
     public sealed class ApiRequestHandler : DelegatingHandler
     {
         private readonly CustomAuthStateProvider _auth;
-        private static readonly string ApiHost = new Uri("https://hipsdontlie.live/").Host;
 
         public ApiRequestHandler(CustomAuthStateProvider auth) => _auth = auth;
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
         {
-            //To avoid we dont attach the token to a malicious url
-            if (request.RequestUri?.Host == ApiHost)
-            {
                 var token = await _auth.GetAccessTokenAsync();
                 if (!string.IsNullOrWhiteSpace(token))
                     request.Headers.Authorization =
                         new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-            }
             request.Headers.Accept.ParseAdd("application/json");
             return await base.SendAsync(request, ct);
         }
